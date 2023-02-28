889cd17cdad17fe0186efb0c82e82da990a00313
Dana Ng <ngdana1@gmail.com>
2023-02-27 16:24:35 -0800

    Setup board

---
+++ src/components/Board.tsx
@@ -0,0 +1,20 @@
+import styles from '@/styles/Board.module.css'
+import Cell from '@/components/Cell';
+
+
+interface Props {
+    rows?: number;
+    columns?: number;
+}
+
+const Board = ({rows = 10, columns = 10} : Props) => {
+    const grid = Array(rows * columns).fill(null)
+
+    return <div className={styles.board}>
+        {grid.map((_, index) => {
+            return <Cell key={`cell-${index}`}/>
+        })}
+    </div>
+}
+
+export default Board;
---
+++ src/components/Cell.tsx
@@ -0,0 +1,7 @@
+import styles from '@/styles/Board.module.css'
+
+const Cell = () => {
+    return <div className={styles.cell}></div>
+}
+
+export default Cell;
