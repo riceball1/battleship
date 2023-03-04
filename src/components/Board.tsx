4970b69995f1f61e8256aee884c59f1f740d0519
Dana Ng <ngdana1@gmail.com>
2023-03-04 00:31:14 -0800

    Add scores and losing message

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -1,7 +1,7 @@
 import styles from '@/styles/Board.module.css';
 import Cell from '@/components/Cell';
 import { useState } from 'react';
-import { getShipColor, Coordinates, Ships } from '@/utils/helper';
+import { getShipColor, Coordinates, Ships, Color } from '@/utils/helper';
 import { generateShipCoordinates } from '@/utils/shipHelper';
 import useInitializeShips from '@/utils/useInitializeShips';
 
@@ -28,25 +28,29 @@ const Board = ({ rows = 10, columns = 10, totalShips = 3 }: Props) => {
   const columnArr = Array(columns).fill(null);
   const [gridState, setGridState] = useState(generateBoardState(rows, columns));
 
+  const [ships, setShips] = useState<Ships[]>([]);
+  const [toastMessage, setToastMessage] = useState('');
+  const [shipSunkState, setShipSunkState] = useState(0);
+  const [score, setScore] = useState(rows * 2); // default turns
+
+  // first initialize ships
+  useInitializeShips(setShips);
+
+  // reset board
   const handleResetBoard = () => {
     setGridState(generateBoardState(rows, columns));
     const newShipsCoordinates = generateShipCoordinates();
     // @ts-ignore
     setShips(newShipsCoordinates);
+    setScore(rows * 2)
   };
-
-  const [ships, setShips] = useState<Ships[]>([]);
-  const [toastMessage, setToastMessage] = useState('');
-  const [shipSunkState, setShipSunkState] = useState(0);
-
-  useInitializeShips(setShips);
-
   const handleMakeTurn = ({
     coordinates,
   }: {
     cellId: string;
     coordinates: Coordinates;
   }) => {
+    if (score === 0) return; // prevent making further turns if game is lost
     const { x, y } = coordinates;
 
     const shipColor = getShipColor({
@@ -54,8 +58,22 @@ const Board = ({ rows = 10, columns = 10, totalShips = 3 }: Props) => {
       coordinates: { x, y },
     });
 
-    // check if the ship is sunk - toast message
-    // check if ship hit what type it is -- toast message
+    // if missed - deduct score
+    if (shipColor === Color.NONE) {
+      let currScore = score - 1;
+      if (currScore === 0) {
+        setScore(currScore);
+        // lose the game
+        // send a notification with a button to reset the game
+      } else {
+        setScore(currScore);
+      }
+    } else {
+      let currScore = score + 1;
+      setScore(currScore);
+      // check if the ship is sunk - toast message - add score
+      // check if ship hit what type it is -- toast message
+    }
 
     // update grid state
     setGridState((prevState) => {
@@ -74,7 +92,8 @@ const Board = ({ rows = 10, columns = 10, totalShips = 3 }: Props) => {
 
   return (
     <>
-      <div></div>
+      <h2>{score}</h2>
+      {score === 0 ? <p>Oh noes! You lost the game!</p> : null}
       <div className={styles.board}>
         {rowArr.map((_, rowIndex) => {
           return columnArr.map((_, columnIndex) => {
